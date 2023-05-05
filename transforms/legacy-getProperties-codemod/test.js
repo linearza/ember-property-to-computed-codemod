54310155e8e9c1bf6cddb4ffa7175ff473ad2a2c
linearza <vanwijk.mc@gmail.com>
2023-05-05 18:19:25 +0200

    chore: linting

--- transforms/legacy-getProperties-codemod/test.js
+++ transforms/legacy-getProperties-codemod/test.js
@@ -2,7 +2,7 @@
 
 const { runTransformTest } = require('codemod-cli');
 
-runTransformTest({ 
+runTransformTest({
   name: 'legacy-getProperties-codemod',
   path: require.resolve('./index.js'),
   fixtureDir: `${__dirname}/__testfixtures__/`,
